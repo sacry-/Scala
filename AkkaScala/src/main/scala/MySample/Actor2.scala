5c4599fe47c72827ecd408021c03e793f473f03e
sacry- <nitsche25@gmail.com>
2014-06-19 18:35:42 +0200

    some minor example

--- AkkaScala/src/main/scala/MySample/Actor2.scala
+++ AkkaScala/src/main/scala/MySample/Actor2.scala
@@ -5,44 +5,85 @@ import akka.pattern.{ask, pipe}
 import scala.concurrent.{ExecutionContext}
 import ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import akka.routing.RoundRobinRouter
+import scala.util.Random
 
 /**
  * Created by sacry on 18/06/14.
  */
-object Manager {
-  def props(magicNumber: Long): Props = Props(new Manager(magicNumber))
-}
+object ActorFactory {
 
-class Manager(x: Long) extends Actor {
-  override def receive: Receive = {
-    case 10 => println("Fuck you!")
-    case _ => println("Manager" + x)
+  case class Order(name: String, amount: Int) {
+    override def toString = "Order(" + name + "," + amount + ")"
+  }
+
+  def randomOrder: Order = {
+    val name = Random.alphanumeric.take(6).mkString
+    val amount = Random.nextInt(10)
+    Order(name, amount)
+  }
+
+  def randomDuration: Long = {
+    (500 + Random.nextInt(2000)) toLong
   }
 }
 
-object Worker {
-  def props(x: Int): Props = Props(new Worker(x))
-}
+object Actor2 extends App {
 
-class Worker(x: Int) extends Actor {
-  override def receive: Receive = {
-    case 10 => {
-      val fu = self.ask("Hello!")(5 seconds)
-      fu pipeTo sender()
-    }
-    case i => println("Worker" + x + " " + i)
-  }
-}
+  import ActorFactory._
 
-object Main extends App {
   val system = ActorSystem("MySystem")
 
-  val mg1 = system.actorOf(Manager.props(100), "Manager1")
-  val mg2 = system.actorOf(Manager.props(101), "Manager2")
+  val newOrder = Request(Order("new", 0), 0L)
+  system.actorOf(Manager.props(5), "Manager1") ! newOrder
 
-  val w1 = system.actorOf(Worker.props(1), "Worker1")
-  val w2 = system.actorOf(Worker.props(2), "Worker2")
-  //w2 ! "Hello!"
- w2.ask(10, mg1)(5 seconds)
+  sealed trait Message
+
+  case class Request(order: Order, time: Long) extends Message
+
+  case class Response(order: Order, time: Long) extends Message
+
+  case object Shutdown extends Message
+
+  object Manager {
+    def props(n: Int): Props = Props(new Manager(n))
+  }
+
+  class Manager(n: Int) extends Actor {
+    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinRouter(n)), name = "myRouter")
+    var hours = 0
+
+    override def receive: Receive = {
+      case Request(Order("new", 0), 0L) => {
+        if (hours < 10) {
+          val (order, t) = (randomOrder, randomDuration)
+          println(s"Requesting ${order}. Will take around ${t} MilliSeconds.")
+          hours += 1
+          workerRouter ! Request(order, t)
+        } else {
+          workerRouter ! "tired"
+        }
+      }
+      case Shutdown => {
+        println("Workers are exhausted!");
+        context.stop(workerRouter)
+        println("Manager still works.. :(")
+      }
+    }
+  }
+
+  class Worker() extends Actor {
+    override def receive: Receive = {
+      case "tired" => {
+        println("Some Sleep at last! :)")
+        sender() ! Shutdown
+      }
+      case Request(order, t) => {
+        println(s"${self.path} starts working on ${order} for ${t} seconds")
+        Thread.sleep(t)
+        sender() ! Request(Order("new", 0), 0L)
+      }
+    }
+  }
 
 }
